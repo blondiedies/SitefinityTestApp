bafad5aebd4c6c7873b8c478d16e6ed269e533a6
Katherine Mendible <kmendible@bnc.com.ve>
2025-11-25 14:29:04 -0400

    Working widget with translation to spanish

---
+++ SitefinityWebApp/Mvc/ViewModels/LaunchViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.SqlServer.Server;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitefinityWebApp.Mvc.ViewModels
+{
+    /// <summary>
+    /// Modelo de información de los datos del vuelo de SpaceX.
+    /// </summary>
+    public class LaunchViewModel
+    {
+        [JsonProperty(PropertyName = "success")]
+        public bool Success { get; set; }
+        [JsonProperty(PropertyName = "flight_number")]
+        public string FlightNumber { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+    }
+}
