07bbcb87855f3abdd4c330417c9f31d64bb2cb6e
Katherine Mendible <kmendible@bnc.com.ve>
2025-11-25 15:30:43 -0400

    connection string fix

---
+++ SitefinityWebApp/Configuration/IntegrationConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+using Telerik.Sitefinity.Configuration;
+using Telerik.Sitefinity.Localization;
+
+namespace SitefinityWebApp.Configuration
+{
+    public class IntegrationConfig : ConfigSection
+    {
+        [ConfigurationProperty("endpoint", IsRequired = true)]
+        public string Endpoint
+        {
+            get
+            {
+                return (string)this["endpoint"];
+            }
+            set
+            {
+                this["endpoint"] = value;
+            }
+        }
+        [ConfigurationProperty("isActive", DefaultValue = true)]
+        [ObjectInfo(Description = "Determines if the endpoint is active or not", Title = "Is Active")]
+        public bool IsActive
+        {
+            get
+            {
+                return (bool)this["isActive"];
+            }
+            set
+            {
+                this["isActive"] = value;
+            }
+        }
+
+    }
+}
