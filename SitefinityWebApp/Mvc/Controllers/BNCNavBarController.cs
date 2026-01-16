eb287e8fecc4e26eb1b03766e9e37585740ef581
Katherine Mendible <kmendible@bnc.com.ve>
2026-01-16 16:25:22 -0400

    BNC Nav Bar (WIP)

---
+++ SitefinityWebApp/Mvc/Controllers/BNCNavBarController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace SitefinityWebApp.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "BNCNavBar", Title = "Barra de navegación", SectionName = "Widgets BNC - Figma")]
+    public class BNCNavBarController : Controller
+    {
+        // GET: BNCNavBar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
